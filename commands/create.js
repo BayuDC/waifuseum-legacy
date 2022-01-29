1febd720e87088a1dec4bdd0c4aebc4b1c3ea553
BayuDC <dev.bayudc@gmail.com>
2022-01-30 00:35:38 +0700

    Update `create` command - prevent duplicate categories - better respond message

--- commands/create.js
+++ commands/create.js
@@ -5,15 +5,16 @@ module.exports = {
     async execute(message, name) {
         if (!name) return await message.channel.send("**I'm a teapot**");
 
-        try {
-            const channel = await message.guild.channels.create(name);
-            await channel.setParent(message.channel.parent);
-
-            await Category.create({ name, channelId: channel.id });
-
-            await message.channel.send('Channel created!');
-        } catch (err) {
-            console.log(err);
+        const category = await Category.findOne({ name });
+        if (category) {
+            return await message.channel.send(`Category **${name}** already exists.`);
         }
+
+        const channel = await message.guild.channels.create(name);
+        await channel.setParent(message.channel.parent);
+
+        await Category.create({ name, channelId: channel.id });
+
+        await message.channel.send(`Category **${name}** successfully created.`);
     },
 };
