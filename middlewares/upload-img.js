3329722f4bef8f60b8b69f648acbfa706cd043ed
BayuDC <dev.bayudc@gmail.com>
2022-01-30 02:46:47 +0700

    Add validation for `upload-img` middleware

--- middlewares/upload-img.js
+++ middlewares/upload-img.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 const multer = require('multer');
+const HttpError = require('../lib/http-error');
 
 module.exports = () => {
     const imgSize = 8 * 1024 * 1024;
@@ -9,7 +10,19 @@ module.exports = () => {
         multer({
             dest: './temp/',
             limits: { fileSize: imgSize },
+            fileFilter(req, file, cb) {
+                if (!imgFormats[file.mimetype]) {
+                    return cb(new multer.MulterError('NOT_AN_IMAGE'), false);
+                }
+
+                cb(null, true);
+            },
         }).single('picture')(req, res, err => {
+            if (err) {
+                if (err.code == 'LIMIT_FILE_SIZE') return next(new HttpError(413, err.message));
+                if (err.code == 'NOT_AN_IMAGE') return next(new HttpError(415, 'File is not an image'));
+                return next(new HttpError(422));
+            }
             if (!req.file) return next();
 
             const file = req.file;
