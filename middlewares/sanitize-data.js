32ca027043a03f1a32576af2c8843426c703b621
BayuDC <dev.bayudc@gmail.com>
2022-01-31 19:06:19 +0700

    Update 'sanitize-data' middleware

--- middlewares/sanitize-data.js
+++ middlewares/sanitize-data.js
@@ -1,15 +1,21 @@
 const Waifuseum = require('../lib/waifuseum');
+const Picture = require('../models/picture');
 
 module.exports = () => {
-    return (req, res, next) => {
-        const category = req.params.category ?? req.query.category ?? req.body.category;
+    return async (req, res, next) => {
+        const id = req.params.id ?? req.query.id ?? req.body.id;
+        const picture = !id || (await Picture.findById(id));
+
+        const category = req.params.category ?? req.query.category ?? req.body.category ?? picture.category;
         const sauce = req.query.sauce ?? req.body.sauce;
         const file = req.file;
 
         const waifuseum = Waifuseum(category, req.app.waifuseum.get(category));
 
         res.locals.waifuseum = waifuseum;
+        res.locals.picture = picture;
         res.locals.data = {
+            id,
             category,
             sauce,
             file,
