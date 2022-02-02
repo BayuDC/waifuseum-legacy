614966ed0a18f8b1d9ff803dd5d0375452473b4e
BayuDC <dev.bayudc@gmail.com>
2022-02-03 06:51:31 +0700

    Update `sanitize-data` middleware

--- middlewares/sanitize-data.js
+++ middlewares/sanitize-data.js
@@ -2,24 +2,40 @@ const Waifuseum = require('../lib/waifuseum');
 const Picture = require('../models/picture');
 
 module.exports = () => {
-    return async (req, res, next) => {
-        const id = req.params.id ?? req.query.id ?? req.body.id;
-        const picture = !id || (await Picture.findById(id));
+    return [
+        (req, res, next) => {
+            const category = req.params.category ?? req.query.category ?? req.body.category;
+            const sauce = req.query.sauce ?? req.body.sauce;
+            const file = req.file;
 
-        const category = req.params.category ?? req.query.category ?? req.body.category ?? picture.category;
-        const sauce = req.query.sauce ?? req.body.sauce;
-        const file = req.file;
+            res.locals.data = { category, sauce, file };
+            next();
+        },
+        async (req, res, next) => {
+            const id = req.params.id ?? req.query.id ?? req.body.id;
+            const picture = await Picture.findById(id);
 
-        const waifuseum = Waifuseum(category, req.app.waifuseum.get(category));
+            res.locals.picture = picture;
+            next();
+        },
+        (req, res, next) => {
+            const category = res.locals.picture?.category ?? res.locals.data.category;
+            const waifuseum = Waifuseum(category, req.app.waifuseum.get(category));
 
-        res.locals.waifuseum = waifuseum;
-        res.locals.picture = picture;
-        res.locals.data = {
-            id,
-            category,
-            sauce,
-            file,
-        };
-        next();
-    };
+            res.locals.waifuseum = waifuseum;
+            next();
+        },
+        (req, res, next) => {
+            const picture = res.locals.picture;
+            const category = res.locals.data.category;
+            const channel = req.app.waifuseum.get(category);
+
+            if (picture && category && picture != category) {
+                res.locals.data.category = { name: category, channel };
+                return next();
+            }
+            res.locals.data.category = undefined;
+            next();
+        },
+    ];
 };
