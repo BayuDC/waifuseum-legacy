564e65125c64e2589b9f71b0859c17ea72328ac2
BayuDC <dev.bayudc@gmail.com>
2022-02-09 00:06:00 +0700

    Update `upload-img` middleware - Allow upload picture from url

--- middlewares/upload-img.js
+++ middlewares/upload-img.js
@@ -1,11 +1,32 @@
-const fs = require('fs/promises');
+const { promises: fs, createWriteStream } = require('fs');
+const { randomBytes } = require('crypto');
+const { isWebUri } = require('valid-url');
 const multer = require('multer');
+const axios = require('axios').default;
 const HttpError = require('../lib/http-error');
 
-module.exports = () => {
-    const imgSize = 8 * 1024 * 1024;
-    const imgFormats = { 'image/jpeg': 'jpg', 'image/png': 'png' };
+const imgSize = 8 * 1024 * 1024;
+const imgFormats = { 'image/jpeg': 'jpg', 'image/png': 'png' };
 
+const downloadImg = url => {
+    return new Promise((resolve, reject) => {
+        axios.get(url, { responseType: 'stream' }).then(res => {
+            const fileName = randomBytes(16).toString('hex');
+            const filePath = './temp/' + fileName;
+            const writer = createWriteStream(filePath);
+            res.data.pipe(writer);
+            res.data.on('end', () => {
+                resolve({
+                    filename: fileName,
+                    path: filePath,
+                    mimetype: res.headers['content-type'],
+                });
+            });
+        });
+    });
+};
+
+module.exports = () => {
     return (req, res, next) => {
         multer({
             dest: './temp/',
@@ -17,11 +38,15 @@ module.exports = () => {
 
                 cb(null, true);
             },
-        }).single('picture')(req, res, err => {
+        }).single('picture')(req, res, async err => {
             if (err) {
                 if (err.code == 'LIMIT_FILE_SIZE') return next(new HttpError(413, err.message));
                 if (err.code == 'NOT_AN_IMAGE') return next(new HttpError(415, 'File is not an image'));
                 return next(new HttpError(422));
+            }
+
+            if (!req.file && isWebUri(req.body.picture)) {
+                req.file = await downloadImg(req.body.picture);
             }
             if (!req.file) return next();
 
