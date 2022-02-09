c772b94e474179076799dd7e3a57c34244c0c78f
BayuDC <dev.bayudc@gmail.com>
2022-02-09 12:00:55 +0700

    Update `upload-img` middleware - Add more validation

--- middlewares/upload-img.js
+++ middlewares/upload-img.js
@@ -10,20 +10,35 @@ const imgFormats = { 'image/jpeg': 'jpg', 'image/png': 'png' };
 
 const downloadImg = url => {
     return new Promise((resolve, reject) => {
-        axios.get(url, { responseType: 'stream' }).then(res => {
-            const fileName = randomBytes(16).toString('hex');
-            const filePath = './temp/' + fileName;
-            const writer = createWriteStream(filePath);
-            res.data.pipe(writer);
-            res.data.on('end', () => {
-                resolve({
-                    filename: fileName,
-                    path: filePath,
-                    mimetype: res.headers['content-type'],
+        axios
+            .get(url, { responseType: 'stream' })
+            .then(res => {
+                const file = {
+                    name: randomBytes(16).toString('hex'),
+                    path: './temp/' + this.name,
+                    type: res.headers['content-type'],
+                    size: res.headers['content-length'],
+                };
+
+                if (!imgFormats[file.type]) throw { code: 'NOT_AN_IMAGE' };
+                if (file.size > imgSize) throw { code: 'LIMIT_FILE_SIZE' };
+
+                res.data.pipe(createWriteStream(file.path));
+                res.data.on('end', () => {
+                    resolve({
+                        path: file.path,
+                        filename: file.name,
+                        mimetype: file.type,
+                    });
                 });
-            });
-        });
+            })
+            .catch(reject);
     });
+};
+const handleErr = (err, next) => {
+    if (err.code == 'LIMIT_FILE_SIZE') return next(new HttpError(413, err.message));
+    if (err.code == 'NOT_AN_IMAGE') return next(new HttpError(415, 'File is not an image'));
+    next(new HttpError(422, 'Unprocessable file'));
 };
 
 module.exports = () => {
@@ -39,28 +54,27 @@ module.exports = () => {
                 cb(null, true);
             },
         }).single('picture')(req, res, async err => {
-            if (err) {
-                if (err.code == 'LIMIT_FILE_SIZE') return next(new HttpError(413, err.message));
-                if (err.code == 'NOT_AN_IMAGE') return next(new HttpError(415, 'File is not an image'));
-                return next(new HttpError(422));
-            }
+            if (err) return handleErr(err, next);
 
             if (!req.file && isWebUri(req.body.picture)) {
-                req.file = await downloadImg(req.body.picture);
+                try {
+                    req.file = await downloadImg(req.body.picture);
+                } catch (err) {
+                    return handleErr(err, next);
+                }
             }
             if (!req.file) return next();
 
             const file = req.file;
             const filePath = `./temp/${file.filename}.${imgFormats[file.mimetype]}`;
 
-            fs.rename(file.path, filePath).then(() => {
-                file.path = filePath;
-                file.destroy = () => {
-                    fs.unlink(file.path);
-                    file.destroy = () => {};
-                };
-                next();
-            });
+            await fs.rename(file.path, filePath);
+            file.path = filePath;
+            file.destroy = () => {
+                fs.unlink(file.path);
+                file.destroy = () => {};
+            };
+            next();
         });
     };
 };
