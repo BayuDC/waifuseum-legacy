cfadede2fcfc28aefcef61787288ce3a92cc47eb
Bayu <62124037+BayuDC@users.noreply.github.com>
2022-03-07 00:21:06 +0700

    Quick fix: disable secure cookid

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,7 @@ module.exports.login = async (req, res, next) => {
     };
     const token = jwt.sign(payload, secret, { expiresIn: maxAge });
 
-    res.cookie('token', token, { httpOnly: true, maxAge: maxAge * 1000, secure: true });
+    res.cookie('token', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(201).send();
 };
 module.exports.logout = (req, res, next) => {
