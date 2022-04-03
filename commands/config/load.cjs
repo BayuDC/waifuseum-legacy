ca3141c4a9726ce06529663e7f9dd34e6368bfc0
BayuDC <dev.bayudc@gmail.com>
2022-04-04 04:43:06 +0700

    Add command for manually load config

--- commands/config/load.cjs
+++ commands/config/load.cjs
@@ -1,5 +1,8 @@
 module.exports = {
     name: 'load',
     desc: 'Load configuration values from database',
-    async execute() {},
+    async execute(message, config) {
+        message.client.configs.set(config.serverId, config);
+        message.channel.send('Configuration loaded');
+    },
 };
