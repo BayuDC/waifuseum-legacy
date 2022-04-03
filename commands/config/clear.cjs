935c2c003f279f1202801a785a68631d5e7134eb
BayuDC <dev.bayudc@gmail.com>
2022-04-04 04:39:09 +0700

    Add command for remove config

--- commands/config/clear.cjs
+++ commands/config/clear.cjs
@@ -1,5 +1,11 @@
+const Config = require('../../models/config');
+
 module.exports = {
     name: 'clear',
     desc: 'Remove all configuration values',
-    async execute() {},
+    async execute(message, config) {
+        await Config.findByIdAndDelete(config._id);
+        message.client.configs.delete(config.serverId);
+        message.channel.send('Configuration removed');
+    },
 };
